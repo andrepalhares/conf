986a996ff271ff90e8c700b8b71547637f0790a3
Andre <seu_email@email.com>
2018-12-13 21:55:24 -0200

    Initial commit

---
+++ includes/footer.php
@@ -0,0 +1,15 @@
+<footer class="site-footer" style="height: 30% !important;">
+	<div class="copywrite-area">
+		<div class="container">
+			
+				<!-- Copywrite Text -->
+				<p align="center"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+					Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+					<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+				<p align="center">
+					Desenvolvido por <a href="http://ijunior.com.br">UFMG Informática Júnior</a>.
+				</p>
+		</div>
+	</div>
+</footer>
+</div>
---
+++ includes/menu.php
@@ -0,0 +1,80 @@
+    <div class="site-mobile-menu">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div> <!-- .site-mobile-menu -->
+    
+    
+    <div class="site-navbar-wrap bg-white">
+<div class="site-navbar-top">
+        <div class="container py-2">
+          <div class="row align-items-center">
+            <div class="col-6">
+              <a href="#" class="p-2 pl-0"><span class="icon-twitter"></span></a>
+              <a href="#" class="p-2 pl-0"><span class="icon-facebook"></span></a>
+              <a href="#" class="p-2 pl-0"><span class="icon-linkedin"></span></a>
+              <a href="#" class="p-2 pl-0"><span class="icon-instagram"></span></a>
+            </div>
+            <div class="col-6">
+              <div class="d-flex ml-auto">
+                <a href="#" class="d-flex align-items-center ml-auto mr-4">
+                  <span class="icon-phone mr-2"></span>
+                  <span class="d-none d-md-inline-block">youremail@domain.com</span>
+                </a>
+                <a href="#" class="d-flex align-items-center">
+                  <span class="icon-envelope mr-2"></span>
+                  <span class="d-none d-md-inline-block">+1 291 2830 302</span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="site-navbar bg-dark">
+        <div class="container py-1">
+          <div class="row align-items-center">
+
+            <div class="col-4 col-md-4 col-lg-8">
+              <nav class="site-navigation text-left" role="navigation">
+                <div class="d-inline-block d-lg-none ml-md-0 mr-auto py-3"><a href="#" class="site-menu-toggle js-menu-toggle text-white"><span class="icon-menu h3"></span></a></div>
+
+                <ul class="site-menu js-clone-nav d-none d-lg-block">
+                  <li class="">
+                    <a href="index.php">Home</a>
+                  </li>
+                  <li><a href="domain.php">Domain</a></li>
+                  <li class="has-children">
+                    <a href="hosting.php">Hosting</a>
+                    <ul class="dropdown arrow-top">
+                      <li><a href="#">Shared Hosting</a></li>
+                      <li><a href="#">Dedicated Hosting</a></li>
+                      <li><a href="#">Premium Hosting</a></li>
+                      <li><a href="#">Cloud Hosting</a></li>
+                      <li class="has-children">
+                        <a href="#">Sub Menu</a>
+                        <ul class="dropdown">
+                          <li><a href="#">Menu One</a></li>
+                          <li><a href="#">Menu Two</a></li>
+                          <li><a href="#">Menu Three</a></li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </li>
+                  <li><a href="blog.php">Blog</a></li>
+                  <li class="active"><a href="contact.php">Contact</a></li>
+                </ul>
+              </nav>
+            </div>
+            <div class="col-8 col-md-8 col-lg-4 text-right">
+              <a href="register.php" class="btn btn-primary btn-outline-primary rounded-0 text-white py-2 px-4">Register</a>
+              <a href="register.php" class="btn btn-primary btn-primary rounded-0 py-2 px-4">Login</a>
+            </div>
+
+
+          </div>
+        </div>
+      </div>
+    </div>
