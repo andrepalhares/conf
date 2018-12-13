fbe8eab79295a0da61a40ad09fc350be4a77e77e
andrepalhares <33261635+andrepalhares@users.noreply.github.com>
2018-12-13 12:18:24 -0200

    Add files via upload

---
+++ pagina-modelo.php
@@ -0,0 +1,20 @@
+<?php
+	include 'includes/header.php';
+	include 'includes/menu.php';
+?>
+	<div class="unit-5 overlay" style="background-image: url('images/hero_bg_1.jpg');">
+		<div class="container text-center">
+			<h2 class="mb-0">Insira o título da página</h2>
+			<!-- ** Usar quando a página for subpágina no dropdown do menu. -->
+			<p class="mb-0 unit-6"><a href="index.html">Início</a> <span class="sep">></span> <span>Página Atual</span></p>
+		</div>
+	</div>
+	<div class="site-section">
+		<div class="container">
+			<p align="justify">Insira um texto aqui.</p>
+		</div>
+	</div>
+<?php
+	include 'includes/footer.php';
+	include 'includes/js.php';
+?>
---
+++ register.php
@@ -0,0 +1,180 @@
+<?php
+	include 'includes/header.php';
+	include 'includes/menu.php';
+?>
+
+    <div class="unit-5 overlay" style="background-image: url('images/hero_bg_1.jpg');">
+      <div class="container text-center">
+        <h2 class="mb-0">Register / Sign in</h2>
+        <p class="mb-0 unit-6"><a href="index.html">Home</a> <span class="sep">></span> <span>Domain</span></p>
+      </div>
+    </div>
+
+
+    <div class="site-section bg-light">
+      <div class="container">
+        <div class="row">
+       
+          <div class="col-md-12 col-lg-6 mb-5">
+          
+            
+            
+            <form action="#" class="p-5 bg-white">
+              <h2 class="mb-4 text-black">Register</h2>
+              <div class="row form-group">
+                <div class="col-md-12 mb-3 mb-md-0">
+                  <label class="font-weight-bold" for="fullname">Full Name</label>
+                  <input type="text" id="fullname" class="form-control rounded-0">
+                </div>
+              </div>
+              <div class="row form-group">
+                <div class="col-md-12">
+                  <label class="font-weight-bold" for="email">Email</label>
+                  <input type="email" id="email" class="form-control rounded-0">
+                </div>
+              </div>
+              <div class="row form-group">
+                <div class="col-md-12">
+                  <label class="font-weight-bold" for="password">Password</label>
+                  <input type="password" id="password" class="form-control rounded-0">
+                </div>
+              </div>
+              <div class="row form-group mb-5">
+                <div class="col-md-12">
+                  <label class="font-weight-bold" for="password2">Re-type Password</label>
+                  <input type="password" id="password2" class="form-control rounded-0">
+                </div>
+              </div>
+
+
+              <div class="row form-group">
+                <div class="col-md-12">
+                  <input type="submit" value="Register" class="btn btn-primary  py-2 px-4 rounded-0">
+                </div>
+              </div>
+
+  
+            </form>
+          </div>
+
+          <div class="col-md-12 col-lg-6 mb-5">
+          
+            
+            
+            <form action="#" class="p-5 bg-white">
+              <h2 class="mb-4 text-black">Sign in</h2>
+              
+              <div class="row form-group">
+                <div class="col-md-12">
+                  <label class="font-weight-bold" for="email2">Email</label>
+                  <input type="email" id="email2" class="form-control rounded-0">
+                </div>
+              </div>
+              <div class="row form-group">
+                <div class="col-md-12">
+                  <label class="font-weight-bold" for="password">Password</label>
+                  <input type="password" id="password" class="form-control rounded-0">
+                </div>
+              </div>
+              
+              <div class="row form-group">
+                <div class="col-md-12">
+                  <label class="font-weight-normal" for="remember">
+                  <input type="checkbox" id="remember">
+                  Remember Me
+                </label>
+                </div>
+              </div>
+
+              <div class="row form-group mb-2">
+                <div class="col-md-12">
+                  <p>No account yet? <a href="#">Register</a>. Forgot password? <a href="#">click here</a></p>
+                </label>
+                </div>
+              </div>
+  
+              <div class="row form-group">
+                <div class="col-md-12">
+                  <input type="submit" value="Login" class="btn btn-primary  py-2 px-4 rounded-0">
+                </div>
+              </div>
+
+  
+            </form>
+          </div>
+
+          
+        </div>
+      </div>
+    </div>
+
+   
+
+
+    <div class="site-section">
+      <div class="container">
+        <div class="row justify-content-center text-center mb-5">
+          <div class="col-md-6" data-aos="fade" >
+            <h2>Frequently Ask Questions</h2>
+          </div>
+        </div>
+        
+
+        <div class="row justify-content-center" data-aos="fade" data-aos-delay="100">
+          <div class="col-md-8">
+            <div class="accordion unit-8" id="accordion">
+            <div class="accordion-item">
+              <h3 class="mb-0 heading">
+                <a class="btn-block" data-toggle="collapse" href="#collapseOne" role="button" aria-expanded="true" aria-controls="collapseOne">What is the name of your company<span class="icon"></span></a>
+              </h3>
+              <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                <div class="body-text">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur quae cumque perspiciatis aperiam accusantium facilis provident aspernatur nisi optio debitis dolorum, est eum eligendi vero aut ad necessitatibus nulla sit labore doloremque magnam! Ex molestiae, dolor tempora, ad fuga minima enim mollitia consequuntur, necessitatibus praesentium eligendi officia recusandae culpa tempore eaque quasi ullam magnam modi quidem in amet. Quod debitis error placeat, tempore quasi aliquid eaque vel facilis culpa voluptate.</p>
+                </div>
+              </div>
+            </div> <!-- .accordion-item -->
+            
+            <div class="accordion-item">
+              <h3 class="mb-0 heading">
+                <a class="btn-block" data-toggle="collapse" href="#collapseTwo" role="button" aria-expanded="false" aria-controls="collapseTwo">How much pay for 3  months?<span class="icon"></span></a>
+              </h3>
+              <div id="collapseTwo" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+                <div class="body-text">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel ad laborum expedita. Nostrum iure atque enim quisquam minima distinctio omnis, consequatur aliquam suscipit, quidem, esse aspernatur! Libero, excepturi animi repellendus porro impedit nihil in doloremque a quaerat enim voluptatum, perspiciatis, quas dignissimos maxime ut cum reiciendis eius dolorum voluptatem aliquam!</p>
+                </div>
+              </div>
+            </div> <!-- .accordion-item -->
+
+            <div class="accordion-item">
+              <h3 class="mb-0 heading">
+                <a class="btn-block" data-toggle="collapse" href="#collapseThree" role="button" aria-expanded="false" aria-controls="collapseThree">Do I need to register?  <span class="icon"></span></a>
+              </h3>
+              <div id="collapseThree" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+                <div class="body-text">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel ad laborum expedita. Nostrum iure atque enim quisquam minima distinctio omnis, consequatur aliquam suscipit, quidem, esse aspernatur! Libero, excepturi animi repellendus porro impedit nihil in doloremque a quaerat enim voluptatum, perspiciatis, quas dignissimos maxime ut cum reiciendis eius dolorum voluptatem aliquam!</p>
+                </div>
+              </div>
+            </div> <!-- .accordion-item -->
+
+            <div class="accordion-item">
+              <h3 class="mb-0 heading">
+                <a class="btn-block" data-toggle="collapse" href="#collapseFour" role="button" aria-expanded="false" aria-controls="collapseFour">Who should I contact in case of support.<span class="icon"></span></a>
+              </h3>
+              <div id="collapseFour" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+                <div class="body-text">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel ad laborum expedita. Nostrum iure atque enim quisquam minima distinctio omnis, consequatur aliquam suscipit, quidem, esse aspernatur! Libero, excepturi animi repellendus porro impedit nihil in doloremque a quaerat enim voluptatum, perspiciatis, quas dignissimos maxime ut cum reiciendis eius dolorum voluptatem aliquam!</p>
+                </div>
+              </div>
+            </div> <!-- .accordion-item -->
+
+          </div>
+          </div>
+        </div>
+      
+      </div>
+    </div>
+
+<?php
+	include 'includes/footer.php';
+	include 'includes/js.php';
+?>
